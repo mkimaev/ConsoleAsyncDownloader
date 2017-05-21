002cc1e915f7d0f1d134f13ff7efd25e2f8595b3
mkimaev <maxim.kimaev@gmail.com>
2017-05-21 21:25:41 +0300

    Add project files.

---
+++ ConsoleAsyncDownloader/MyDownloader.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Diagnostics;
+using System.Threading;
+using System.IO;
+
+namespace ConsoleAsyncDownloader
+{
+    class MyDownloader
+    {
+        /// <summary>
+        /// синхронный метод скачивания
+        /// </summary>
+        public string UriDownloadText(string url)
+        {
+            string data = null;
+            WebClient wc = new WebClient();
+            for (int i = 0; i < 50; i++) // рисуем точки, чтобы увидеть разницу - будет задержка при скачке
+            {
+                Console.Write(".");
+                Thread.Sleep(50);
+                if (i == 25)
+                {
+                    Console.Write("|dowloading...|");
+                    data = wc.DownloadString(url); // метод загрузки
+                    continue;
+                }
+            }
+            return data;
+
+        }
+
+        /// <summary>
+        /// асинхронный метод скачивания
+        /// </summary>
+        public async Task<string> UriDownloadTextAsync(string url)
+        {
+            WebClient wc2 = new WebClient();
+            Task<string> taskAsync = null;
+            for (int i = 0; i < 50; i++)
+            {
+                Console.Write("."); // рисуем точки, чтобы увидеть разницу - задержки не будет
+                Thread.Sleep(50);
+                if (i == 25)
+                {
+                    Console.Write("|dowloading...|(без задержки)");
+                    taskAsync = wc2.DownloadStringTaskAsync(url); // асинхронный метод загрузки
+                    continue;
+                }
+            }
+            return await taskAsync; ;
+        }
+    }
+}
---
+++ ConsoleAsyncDownloader/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Diagnostics;
+using System.Threading;
+using System.IO;
+
+namespace ConsoleAsyncDownloader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Программа для скачивания книги Приключения Тома Сойера\n");
+                string source = "http://www.gutenberg.org/files/74/74-0.txt";
+                MyDownloader download = new MyDownloader();
+                Stopwatch sw = new Stopwatch();
+                Stopwatch sw2 = new Stopwatch();
+                Console.WriteLine("нажмите s - для скачивания книги в синхронном режите нажмите");
+                Console.WriteLine("нажмите as - для скачивания в асинхронном режите нажмите");
+                string command = Console.ReadLine().ToLower();
+                switch (command)
+                {
+                    case "s":
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        sw.Start();
+                        string book = download.UriDownloadText(source);
+                        using (StreamWriter str = new StreamWriter("Tom_Soyer.txt"))
+                        {
+                            str.Write(book);
+                            //str.Close();
+                            Console.WriteLine("записано в Tom_Soyer.txt");
+                        }
+                        sw.Stop();
+                        Console.WriteLine("Операция UriDownloadText завершена! Длительность: {0} мс\n", sw.Elapsed);
+                        Console.ResetColor();
+                        break;
+                    case "as":
+                        Console.ForegroundColor = ConsoleColor.DarkGreen;
+                        sw2.Start();
+                        Task<string> anytask = download.UriDownloadTextAsync(source);
+                        string bookAsync = anytask.Result;
+                        using (StreamWriter str2 = new StreamWriter("Tom_SoyerAsync.txt"))
+                        {
+                            str2.Write(bookAsync);
+                            Console.WriteLine("записано в Tom_SoyerAsync.txt");
+                        }
+                        sw2.Stop();
+                        Console.WriteLine("Операция UriDownloadTextAsync завершена! Длительность: {0} мс\n", sw2.Elapsed);
+                        Console.ResetColor();
+                        break;
+                }
+            }
+        }
+    }
+}
